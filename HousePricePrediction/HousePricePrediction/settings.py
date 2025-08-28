bb485ef8efc29d9bc5dccf5f2dba631ca963e9bc
Ghislian Chigozie <chigozieghislian@gmail.com>
2025-08-28 17:34:58 +0100

    some few changes

--- HousePricePrediction/HousePricePrediction/settings.py
+++ HousePricePrediction/HousePricePrediction/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mwc(wk6v68xo6)vspd1#v*7@9=x6zh_nm-gtnh60&h&#a%1*b1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["housepriceprediction-vrcs.onrender.com"]
 
 
 # Application definition
